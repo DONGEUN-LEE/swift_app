25bc61af8bab18421a412600e9054df2dea9f272
DONGEUN-LEE <adsl2109@gmail.com>
2021-04-10 13:55:23 +0900

    Initial Commit

---
+++ swift_app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swift_app
+//
+//  Created by 이동은 on 2021/04/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swift_app/swift_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_appApp.swift
+//  swift_app
+//
+//  Created by 이동은 on 2021/04/10.
+//
+
+import SwiftUI
+
+@main
+struct swift_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
